85fdba2d34fc31af7ef1593fdc908b8dce4657be
Esmeralda Gallardo <gallardo.esmeralda05@gmail.com>
2022-07-13 22:49:48 -0300

    Deleted unused pages.

--- src/pages/Donate.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<q-page class="flex flex-center">
-		<h5>Donate</h5>
-	</q-page>
-</template>
--- src/pages/HowItWorks.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<q-page class="flex flex-center">
-		<h5>How does Lovebets work?</h5>
-	</q-page>
-</template>
-
-
